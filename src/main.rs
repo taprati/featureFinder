2cb27d13b6fa68c16cce0857f10976be905bf448
taprati <tyleraprati@gmail.com>
2021-03-21 13:23:31 -0400

    added tsv parser

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,12 @@
 // 
 
 
-// use std::path::PathBuf;
 use structopt::StructOpt;
 
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-   
+
     // Input file
     #[structopt(short = "i", long = "input", parse(from_os_str))]
     input: std::path::PathBuf,
@@ -29,7 +28,7 @@ struct Cli {
 
     // Start index
     #[structopt(short = "s", long = "start")]
-    start: u32, 
+    start: u32,
 
     // End index
     #[structopt(short = "e", long = "end")]
@@ -40,16 +39,45 @@ struct Cli {
 
 fn main() {
     // Parse args 
-    let cli = Cli::from_args();
+    let args = Cli::from_args();
 
     // Print arg to debug
-    println!("{:?}", cli);
+    println!("{:?}", args);
     
-    let chromosome = &cli.chrom;
-    let feature = &cli.feature;
-    let start = &cli.start;
-    let end = &cli.end;
+    // put args into variables
+    let input = std::fs::read_to_string(args.input)
+                            .expect("File not Found!");
+    let chromosome = &args.chrom;
+    let feature = &args.feature;
+    let start = &args.start;
+    let end = &args.end;
+
+    // parse file 
+    'outer: for line in input.lines() {
+        // parse line by tabs
+        let fields: Vec<&str> = line.split("\t").collect();
+
+        // TODO: is there a better way to do this?
+        // check for header
+        let head = &fields[0].chars().nth(0).unwrap();
+        // if header skip
+        if head == &'#' {
+            continue 'outer ;
+        }
+        
+        // parse fields into variables
+        let chrom = fields[0].trim();
+
+        // TODO: make the whole comparison
+        if chrom == chromosome {
+            println!("{}", chrom);
+        }
+
+        //println!("{}", chrom);
+            
+    }
 
 }
+
 
 
